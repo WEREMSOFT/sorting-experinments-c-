c44e37e072f3088f0f2c6d85f8db83a4fdd6e0e4
WEREMSOFT <pablo.weremczuk@gmail.com>
2019-06-27 21:23:07 +0200

    integrated spine into an object

--- src/game/screens/dialog.hpp
+++ src/game/screens/dialog.hpp
@@ -5,17 +5,64 @@
 #pragma once
 #include <spine/spine-sfml.h>
 
+struct SpineGameObject: GameObject {
+    uint state = 0;
+    std::shared_ptr<spine::SkeletonDrawable> drawable;
+    std::shared_ptr<spine::SkeletonData> raptorSkeletonData;
+    std::shared_ptr<spine::Atlas> raptorAtlas;
+    spine::SFMLTextureLoader spineTextureLoader;
+    spine::SkeletonJson *raptorSkeleton;
+
+    SpineGameObject(const char* atlasFileName, const char* jsonFileName){
+        raptorAtlas = std::make_shared<spine::Atlas>(atlasFileName, &spineTextureLoader);
+
+        raptorSkeleton = new spine::SkeletonJson(raptorAtlas.get());
+        raptorSkeleton->setScale(1);
+        auto skeletonData = raptorSkeleton->readSkeletonDataFile(jsonFileName);
+        if (!skeletonData) {
+            printf("%s\n", raptorSkeleton->getError().buffer());
+            exit(0);
+        }
+
+        raptorSkeletonData = std::shared_ptr<spine::SkeletonData>(skeletonData);
+
+
+        drawable = std::shared_ptr<spine::SkeletonDrawable>(new spine::SkeletonDrawable(raptorSkeletonData.get()));
+        drawable.get()->timeScale = 1;
+        drawable.get()->setUsePremultipliedAlpha(true);
+        spine::Skeleton* skeleton = drawable.get()->skeleton;
+        skeleton->setPosition(500, 700);
+        skeleton->updateWorldTransform();
+
+        drawable.get()->state->setAnimation(0, "stand", true);
+
+    }
+
+    void setPosition(float x, float y){
+        drawable.get()->skeleton->setPosition(x, y);
+    }
+
+    virtual void draw(sf::RenderTarget& renderTarget, sf::RenderStates renderStates) const {
+        renderTarget.draw(*drawable.get(), renderStates);
+    }
+
+    virtual void update(float dt) {
+        drawable.get()->update(dt);
+    }
+
+    void setposition(sf::Vector2f position) { drawable.get()->skeleton->setPosition(position.x, position.y); };
+};
+
+
 struct Dialog: Screen {
     sf::IntRect textureRect;
     Menu menu;
     GameObject character;
-    std::shared_ptr<spine::SkeletonDrawable> raptor;
-    std::shared_ptr<spine::SkeletonData> raptorSkeletonData;
-    std::shared_ptr<spine::Atlas> raptorAtlas;
-    std::shared_ptr<spine::SFMLTextureLoader> spineTextureLoader;
-    spine::SkeletonJson *raptorSkeleton;
+    std::unique_ptr<SpineGameObject> dragon;
+
 
     Dialog(Context& context): Screen(context.textureHolder->get(Textures::BACKGROUND_DIALOG), context), menu(context, *this) {
+        dragon = std::make_unique<SpineGameObject>("assets/spine/dragon/NewDragon.atlas", "assets/spine/dragon/NewDragon.json");
 
         menu.offset.y = 250;
         context.textureHolder->get(Textures::TITLE_BACKGROUND_TILE).setRepeated(true);
@@ -38,43 +85,9 @@ struct Dialog: Screen {
         layers[LAYER_MIDDLE].addChild(&menu);
         character.sprite.setTexture(context.textureHolder->get(Textures::PANDA));
         character.sprite.setScale(0.45, 0.45);
-        layers[LAYER_BACKGROUND].addChild(&character);
 
-
-        // SPINE
-//        "data/raptor-pro.json", "data/raptor-pro.skel", "data/raptor-pma.atlas", 0.5f);
-        spineTextureLoader = std::make_shared<spine::SFMLTextureLoader>();
-
-
-        raptorAtlas = std::make_shared<spine::Atlas>("assets/spine/raptor/raptor-pma.atlas", spineTextureLoader.get());
-
-        auto test = raptorAtlas.get();
-
-//        raptorSkeleton = std::make_shared<spine::Skeleton>(test);
-
-//        spine::SkeletonJson json(raptorAtlas.get());
-        raptorSkeleton = new spine::SkeletonJson(raptorAtlas.get());
-        raptorSkeleton->setScale(1);
-        auto skeletonData = raptorSkeleton->readSkeletonDataFile("assets/spine/raptor/raptor-pro.json");
-        if (!skeletonData) {
-            printf("%s\n", raptorSkeleton->getError().buffer());
-            exit(0);
-        }
-
-        raptorSkeletonData = std::shared_ptr<spine::SkeletonData>(skeletonData);
-
-
-        raptor = std::shared_ptr<spine::SkeletonDrawable>(new spine::SkeletonDrawable(raptorSkeletonData.get()));
-        raptor.get()->timeScale = 1;
-        raptor.get()->setUsePremultipliedAlpha(true);
-//
-        spine::Skeleton* skeleton = raptor.get()->skeleton;
-        skeleton->setPosition(320, 1000);
-        skeleton->updateWorldTransform();
-
-        raptor.get()->state->setAnimation(0, "walk", true);
-//        raptor.get()->state->addAnimation(1, "gun-grab", false, 2);
-
+        layers[LAYER_BACKGROUND].addChild(dragon.get());
+//        layers[LAYER_BACKGROUND].addChild(&character);
 
     }
 
@@ -83,7 +96,7 @@ struct Dialog: Screen {
         if(isKeyDown(keys::BackSpace)) {
             passToStateTransitionOut(MAIN_MENU);
         }
-        raptor.get()->update(dt);
+
     }
 
     static void onMenuItemSelected(Screen& screen, int itemSelected) {
@@ -92,7 +105,6 @@ struct Dialog: Screen {
 
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
         Screen::draw(target, states);
-        target.draw(*raptor.get());
     }
 };
 
