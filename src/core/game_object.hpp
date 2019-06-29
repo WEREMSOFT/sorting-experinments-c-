efc8b9803219d371fdf22dbd1aea270c52b7fb07
WEREMSOFT <pablo.weremczuk@gmail.com>
2019-06-29 13:42:39 +0200

    Created animated game object

--- src/core/game_object.hpp
+++ src/core/game_object.hpp
@@ -3,50 +3,59 @@
 //
 
 #pragma once
-#include <spine/spine-sfml.h>
 
-struct GameObject: sf::Drawable, sf::NonCopyable {
+#include <spine/spine-sfml.h>
+#include "resources/animation.hpp"
+
+struct GameObject : sf::Drawable, sf::NonCopyable {
     uint state = 0;
     sf::Sprite sprite;
     uint childCounter = 0;
-    GameObject* children[10] = {0};
+    GameObject *children[10] = {0};
 
-    GameObject(const sf::Texture& texture){
+    GameObject(const sf::Texture &texture) {
         sprite.setTexture(texture);
     }
 
-    GameObject(){
+    GameObject() {
     }
 
-    void setPosition(float x, float y){
+    void setPosition(float x, float y) {
         sprite.setPosition(x, y);
     }
 
-    virtual void draw(sf::RenderTarget& renderTarget, sf::RenderStates renderStates) const {
+    virtual void draw(sf::RenderTarget &renderTarget, sf::RenderStates renderStates) const {
         renderTarget.draw(sprite, renderStates);
-        for(int i = 0; i < childCounter; i++){
+        for (int i = 0; i < childCounter; i++) {
             renderTarget.draw(*children[i], renderStates.transform * sprite.getTransform());
         }
     }
 
-    void addChild(GameObject* gameObject){
+    void addChild(GameObject *gameObject) {
         children[childCounter++] = gameObject;
     }
 
     virtual void update(float dt) {
-        for(int i = 0; i < childCounter; i++){
+        for (int i = 0; i < childCounter; i++) {
             children[i]->update(dt);
         }
     }
 
-    const sf::Vector2f& getposition() { return sprite.getPosition(); };
+    const sf::Vector2f &getposition() { return sprite.getPosition(); };
+
     const void setposition(sf::Vector2f position) { sprite.setPosition(position); };
 
 
 };
 
+struct AnimatedGameObject : public GameObject {
 
-struct SpineGameObject: GameObject {
+    AnimatedGameObject(const sf::Texture &texture) : GameObject(texture) {
+
+    }
+};
+
+struct SpineGameObject : GameObject {
     uint state = 0;
     std::shared_ptr<spine::SkeletonDrawable> drawable;
     std::shared_ptr<spine::SkeletonData> raptorSkeletonData;
@@ -54,7 +63,7 @@ struct SpineGameObject: GameObject {
     spine::SFMLTextureLoader spineTextureLoader;
     spine::SkeletonJson *raptorSkeleton;
 
-    SpineGameObject(const char* atlasFileName, const char* jsonFileName){
+    SpineGameObject(const char *atlasFileName, const char *jsonFileName) {
         raptorAtlas = std::make_shared<spine::Atlas>(atlasFileName, &spineTextureLoader);
 
         raptorSkeleton = new spine::SkeletonJson(raptorAtlas.get());
@@ -71,7 +80,7 @@ struct SpineGameObject: GameObject {
         drawable = std::shared_ptr<spine::SkeletonDrawable>(new spine::SkeletonDrawable(raptorSkeletonData.get()));
         drawable.get()->timeScale = 1;
         drawable.get()->setUsePremultipliedAlpha(true);
-        spine::Skeleton* skeleton = drawable.get()->skeleton;
+        spine::Skeleton *skeleton = drawable.get()->skeleton;
         skeleton->setPosition(500, 700);
         skeleton->updateWorldTransform();
 
@@ -79,11 +88,11 @@ struct SpineGameObject: GameObject {
 
     }
 
-    void setPosition(float x, float y){
+    void setPosition(float x, float y) {
         drawable.get()->skeleton->setPosition(x, y);
     }
 
-    virtual void draw(sf::RenderTarget& renderTarget, sf::RenderStates renderStates) const {
+    virtual void draw(sf::RenderTarget &renderTarget, sf::RenderStates renderStates) const {
         renderTarget.draw(*drawable.get(), renderStates);
     }
 
